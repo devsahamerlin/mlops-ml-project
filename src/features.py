fb0255a3a0f6ba719353310d0d2f4d5fc6ac51b6
devsahamerlin <37698308+devsahamerlin@users.noreply.github.com>
2026-01-11 09:26:35 +0400

    Feature improve preprocessing pipeline
    * feat: improve preprocessing pipeline
    
    * docs: update readme with tags and release commands

--- src/features.py
+++ src/features.py
@@ -1,6 +1,8 @@
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import FunctionTransformer
+
 def build_numeric_preprocess():
     """
     Prétraitement minimal (baseline) :
@@ -10,4 +12,15 @@ def build_numeric_preprocess():
     return Pipeline(steps=[
         ("imputer", SimpleImputer(strategy="median")),
         ("scaler", StandardScaler()),
+    ])
+
+
+def _clip(X):
+    return X.clip(-3, 3)
+
+def build_numeric_preprocess():
+    return Pipeline(steps=[
+        ("imputer", SimpleImputer(strategy="median")),
+        ("scaler", StandardScaler()),
+        ("clip", FunctionTransformer(_clip)),
     ])
